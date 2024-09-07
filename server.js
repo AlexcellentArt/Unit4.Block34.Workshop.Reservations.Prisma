9fa0b3d8f16e0cca15676670e94a841dc616c001
Alexis <alexcellentart@gmail.com>
2024-09-06 23:51:33 -0400

    Get funcs added

--- server.js
+++ server.js
@@ -1,16 +1,49 @@
-// const express = require("express");
-// const prisma = require("./prisma");
-// const app = express();
-// const PORT = 3000;
+const express = require("express");
+const prisma = require("./prisma");
+const app = express();
+const PORT = 3000;
 
-// // body parsing middleware
-// app.use(express.json());
-// app.use(require("morgan")("dev"))
-// // error handling middleware
-// app.use((error,req,res,next)=>{
-//     res.status(res.status||500).send({error:error})
-// })
+// body parsing middleware
+app.use(express.json());
+app.use(require("morgan")("dev"))
+// error handling middleware
+app.use((error,req,res,next)=>{
+    res.status(res.status||500).send({error:error})
+})
 
-// app.listen(PORT, () => {
-//     console.log(`Server listening on port ${PORT}`)
-// })
+// GET
+
+// Get Customer
+app.get("/api/customers", async (req, res, next) => {
+    try {
+        const customers = await prisma.customer.findMany();
+        res.json(customers)
+    } catch (error) {
+        next(error)
+    }
+})
+
+// Get Restaurant
+app.get("/api/restaurants", async (req, res, next) => {
+    try {
+        const restaurants = await prisma.restaurant.findMany();
+        res.json(restaurants)
+    } catch (error) {
+        next(error)
+    }
+})
+
+// Get Reservation
+app.get("/api/customers", async (req, res, next) => {
+    try {
+        const reservation = await prisma.reservation.findMany();
+        res.json(reservation)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
+})
