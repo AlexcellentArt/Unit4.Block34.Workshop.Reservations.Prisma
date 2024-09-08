2f62b8841324c2853602a50edb69a2c12203e0cd
Alexis <alexcellentart@gmail.com>
2024-09-07 22:35:59 -0400

    MVP finished. Added id get funcs for base routes in victory lap.

--- server.js
+++ server.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const prisma = require("./prisma");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 // body parsing middleware
 app.use(express.json());
 app.use(require("morgan")("dev"));
-// error handling middleware
-app.use((error, req, res, next) => {
-  res.status(res.status || 500).send({ error: error });
-});
+
 
 // GET
 
@@ -22,6 +19,22 @@ app.get("/api/customers", async (req, res, next) => {
     next(error);
   }
 });
+// get specific customer
+app.get("/api/customers/:customerId", async (req, res, next) => {
+    try {
+    const id = +req.params.customerId;
+    const exists = await prisma.customer.findFirst({ where: { id } });
+    if (!exists) {
+        return next({
+        status: 404,
+        message: `Unable to find customer with customerId of ${id}`,
+        });
+    }
+      res.json(exists);
+    } catch (error) {
+      next(error);
+    }
+  });
 
 // Get Restaurant
 app.get("/api/restaurants", async (req, res, next) => {
@@ -33,8 +46,25 @@ app.get("/api/restaurants", async (req, res, next) => {
   }
 });
 
+// get specific restaurant
+app.get("/api/restaurants/:restaurantId", async (req, res, next) => {
+    try {
+    const id = +req.params.restaurantId;
+    const exists = await prisma.restaurant.findFirst({ where: { id } });
+    if (!exists) {
+        return next({
+        status: 404,
+        message: `Unable to find restaurant with restaurantId of ${id}`,
+        });
+    }
+      res.json(exists);
+    } catch (error) {
+      next(error);
+    }
+  });
+
 // Get Reservation
-app.get("/api/customers", async (req, res, next) => {
+app.get("/api/reservations", async (req, res, next) => {
   try {
     const reservation = await prisma.reservation.findMany();
     res.json(reservation);
@@ -43,17 +73,46 @@ app.get("/api/customers", async (req, res, next) => {
   }
 });
 
+// get specific reservation
+app.get("/api/reservations/:reservationId", async (req, res, next) => {
+    try {
+    const id = +req.params.reservationId;
+    const exists = await prisma.reservation.findFirst({ where: { id } });
+    if (!exists) {
+        return next({
+        status: 404,
+        message: `Unable to find reservation with reservationId of ${id}`,
+        });
+    }
+      res.json(exists);
+    } catch (error) {
+      next(error);
+    }
+  });
+
 // POST Reservation
-app.get("/api/customers/:customerId/reservations", async (req, res, next) => {
+app.post("/api/customers/:customerId/reservations", async (req, res, next) => {
   try {
+    // verify customer exists
+    const customerId = +req.params.customerId;
+    const customerExists = await prisma.customer.findFirst({ where: { id: customerId } });
+    if (!customerExists) {
+        return next({
+        status: 404,
+        message: `Unable to find customer with customerId of ${customerId}`,
+        });
+    }
+    // deconstruct body and create new reservation
     const { restaurantId, date, partyCount } = req.body;
     const reservation = await prisma.reservation.create({
       data: {
         restaurantId,
         date,
         partyCount,
+        customerId
       },
     });
+    //send
     res.sendStatus(201);
     res.json(reservation);
   } catch (error) {
@@ -63,21 +122,36 @@ app.get("/api/customers/:customerId/reservations", async (req, res, next) => {
 // DELETE Reservation
 app.delete("/api/customers/:customerId/reservations/:id", async (req, res, next) => {
   try {
-    const id = +req.params.id;
-    const exists = await prisma.reservation.findFirst({ where: { id } });
-    if (!exists) {
-      return next({
+    // verify customer exists
+    const customerId = +req.params.customerId;
+    const customerExists = await prisma.customer.findFirst({ where: { id: customerId } });
+    if (!customerExists) {
+        return next({
         status: 404,
-        message: `Unable to find reservation with id of ${id}`,
-      });
+        message: `Unable to find customer with customerId of ${customerId}`,
+        });
     }
-    await prisma.reservation.delete({ where: { id } });
+    // verify reservation exists
+    const reservationId = +req.params.id;
+    const reservationExists = await prisma.reservation.findFirst({ where: { id: reservationId } });
+    if (!reservationExists) {
+        return next({
+        status: 404,
+        message: `Unable to find reservation with id of ${reservationId}`,
+        });
+    }
+    //send
+    await prisma.reservation.delete({ where: { id: reservationId } });
     res.sendStatus(204);
   } catch (error) {
     next(error);
   }
 });
-
+// error handling middleware, which MUST BE AFTER EVERYTHING BUT LISTEN
+app.use((error, req, res, next) => {
+    console.log("MADE IT TO ERROR HANDLER")
+    res.status(Math.floor(res.status) || 500).send({ error: error });
+  });
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
